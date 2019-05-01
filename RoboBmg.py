e54a5950d5bd5b79a2ed349052a38a568a4441e4
FelipeSantosSouza <felipesnt13@gmail.com>
2019-05-01 20:16:52 -0300

    'inicio'

---
+++ RoboBmg.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+
+dominio = "https://www.bmginvestdigital.com.br/Home/investimentos"
+
+def capturarConterudoEsp():
+    opt = Options()
+    opt.headless = True
+    driver = webdriver.Firefox(options=opt, executable_path=r'geckodriver.exe')
+    driver.get(dominio)
+    elemento = driver.find_element(By.CLASS_NAME, "product")
+    gerarInvestimentoEsp(elemento)
+    driver.quit()
+
+def gerarInvestimentoEsp(elemento):
+    elementos = elemento.find_elements(By.CLASS_NAME, "product__details__list")
+    for e in elementos:
+        try:
+            texto = e.text
+            print("-----")
+            prazo = texto[texto.index("Prazo:")+len("Prazo:"):texto.index("Taxa*:")-1]
+            print(prazo)
+        except:
+            print("Investimento Nulo!")
+
+capturarConterudoEsp()
