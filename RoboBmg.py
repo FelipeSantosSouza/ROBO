f05130cd638743cf9867238363d437d6353da986
FelipeSantosSouza <felipesnt13@gmail.com>
2019-05-01 21:05:58 -0300

    'a'

--- RoboBmg.py
+++ RoboBmg.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-
+from pyvirtualdisplay import Display
 dominio = "https://www.bmginvestdigital.com.br/Home/investimentos"
 
 def capturarConterudoEsp():
+    display = Display(visible=0, size=(1024, 768))
+    display.start()
     opt = Options()
     opt.headless = True
     driver = webdriver.Firefox(options=opt, executable_path=r'/opt/geckodriver')
@@ -12,6 +14,7 @@ def capturarConterudoEsp():
     elemento = driver.find_element(By.CLASS_NAME, "product")
     gerarInvestimentoEsp(elemento)
     driver.quit()
+    display.stop()
 
 def gerarInvestimentoEsp(elemento):
     elementos = elemento.find_elements(By.CLASS_NAME, "product__details__list")
