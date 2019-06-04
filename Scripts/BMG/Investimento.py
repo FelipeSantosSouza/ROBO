cdf3ecb533b6752510e2028ca7ba6397451b8e2c
Felipe Santos de Souza <felipesnt13@gmail.com>
2019-06-04 09:32:31 -0300

    Update Investimento.py

--- Scripts/BMG/Investimento.py
+++ Scripts/BMG/Investimento.py
@@ -1,10 +1,10 @@
 class Investimento:
-  def __init__(self, prazo, dominio, rentabilidade, aplicacao_min, ir, liquidez, tipo):
+  def __init__(self, prazo, dominio, rentabilidade, valorMin, ir, liquidez, tipo):
     self.dominio = dominio
     self.prazo = prazo
     self.tipo = tipo
     self.rentabilidade = rentabilidade
-    self.aplicacao_min = aplicacao_min
+    self.valorMin = valorMin
     self.ir = ir
     self.liquidez = liquidez
 
