b8dfc36afbb58d1a62f501e60c939dbea54fc556
FelipeSantosSouza <felipesnt13@gmail.com>
2019-06-18 00:46:58 -0300

    Rafael Barril

--- Scripts/BMG/Gravar.py
+++ Scripts/BMG/Gravar.py
@@ -12,5 +12,6 @@ headers = {'Content-type': 'application/json'}
 
 url = 'http://ec2-52-14-112-85.us-east-2.compute.amazonaws.com:9000/insertInvetimento'
 r = requests.post(url, json=json_data, headers=headers)
-
+print("Saida do POST")
+print(r.text)
 
