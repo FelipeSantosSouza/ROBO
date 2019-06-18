968ec0aac1efd0d11d1f50b8bf0761173c94ed62
FelipeSantosSouza <felipesnt13@gmail.com>
2019-06-18 00:28:57 -0300

    Rafael Barril

--- Scripts/BMG/Gravar.py
+++ Scripts/BMG/Gravar.py
@@ -1,8 +1,16 @@
 import requests
 import urllib3
+import json
+
+with open('jsonBmg.json') as json_file:
+    json_data = json.load(json_file)
+
+for linha in json_data:
+    print(linha)
+
 headers = {'Content-type': 'application/json'}
 
 url = 'http://ec2-52-14-112-85.us-east-2.compute.amazonaws.com:9000/insertInvetimento'
-r = requests.post(url, data=open('jsonBmg.json', 'rb').read(), headers=headers)
+r = requests.post(url, data=json_data, headers=headers)
 
 
