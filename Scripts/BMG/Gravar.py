ed837d9340b2154f07f4935601a3cc1a383e6830
FelipeSantosSouza <felipesnt13@gmail.com>
2019-06-18 00:33:46 -0300

    Rafael Barril

--- Scripts/BMG/Gravar.py
+++ Scripts/BMG/Gravar.py
@@ -11,6 +11,6 @@ for linha in json_data:
 headers = {'Content-type': 'application/json'}
 
 url = 'http://ec2-52-14-112-85.us-east-2.compute.amazonaws.com:9000/insertInvetimento'
-r = requests.post(url, data=json_data, headers=headers)
+r = requests.post(url, json=json_data, headers=headers)
 
 
